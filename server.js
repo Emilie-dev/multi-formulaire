ff879b55690037773cccd1e8c8d94d40ee78ef71
Chauvin <chauvin-e@orange.fr>
2017-05-12 17:12:07 +0200

    add validator

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var nodefs = require('fs');
+var validator = require('validator');
 
 //express
 app.listen(3000, function () {
@@ -13,10 +14,44 @@ app.use(express.static(__dirname + '/'));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-//jade-pug
+//jade
 app.set('views', './views');
 app.set('view engine', 'jade');
 
+function checkcoordinate(coordinate) {
+	var error = {};
+
+	if (!validator.isAlpha(page1.first_name)) {
+		error.first_name = "Pas de caractéres spéciaux!";
+	}
+	if (!validator.last_name(page1.last_name)) {
+		error.last_name = "Pas de caractéres spéciaux!";
+	}
+	if (validator.isEmpty(page1.first_name)) {
+		error.first_name = "Champs obligatoire!";
+	}
+	if (validator.isEmpty(page1.last_name)) {
+		error.last_name = "Champs obligatoire!";
+	}
+	if (validator.isEmpty(page1.age)) {
+		error.age = "Champs obligatoire!";
+	}
+	if (validator.isEmpty(page1.address)) {
+		error.address = "Champs obligatoire!";
+	}
+	return error;
+};
+
 app.get('/coordinate', function (req, res) {
-	res.render('page1', {});
+	if(page1.isEmpty(req.body)) {
+		res.render('page2', {});
+	} else {
+		res.render('page1', {
+			first_name: req.body.first_name,
+			last_name: req.body.last_name,
+			age: req.body.age,
+			address: req.body.address,
+			error: checkcoordinate(req.body),
+		})
+	}
 });
